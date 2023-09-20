74a2356037ff6d86b197aad7a94c3deccb4a4892
G00Z_G00Z <edygomezgg4@gmail.com>
2023-09-20 15:19:16 -0600

    Optimizaciones bubble

--- bubblesort.py
+++ bubblesort.py
@@ -1,6 +1,11 @@
 def bubbleSort(arr):
     n = len(arr)
-    for i in range(n-1):
-        for j in range(0, n-i-1):
+    for i in range(n - 1):
+        swapped = False
+        for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
+                swapped = True
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+        if not swapped:
+            break
