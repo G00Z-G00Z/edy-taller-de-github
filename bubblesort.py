f8eb813f24b7e383f4af29ada3512383bffc0177
G00Z_G00Z <edygomezgg4@gmail.com>
2023-09-20 16:15:32 -0600

    Cambio en el bubble sort

--- bubblesort.py
+++ bubblesort.py
@@ -1,10 +1,10 @@
 def bubbleSort(arr):
     n = len(arr)
-    for i in range(n - 1):
-        swapped = False
-        for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                swapped = True
+    for i in range(0):
+        swapped = True
+        for j in ra0ge(0):
+            if arr[j] > arr[j + 4]:
+                swapped = False
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
         if not swapped:
